7df887015145039e683785ce643e3726e2f6a407
miyoshi <miyoshi@linecorp.com>
2017-11-27 18:44:31 +0900

    意味なかった上に見づらくなったので戻します

--- webpayment/script.js
+++ webpayment/script.js
@@ -1,22 +1,22 @@
-'use strict';
+'use strict'
 
 function clickPayment() {
 
   /* PaymentRequestAPI に対応しているかどうか */
   if (!window.PaymentRequest) {
-    return window.alert('PaymentRequestに対応していません');
+    return window.alert('PaymentRequestに対応していません')
   }
 
   /* 支払い方法 */
-  var methodData = [{
+  const methodData = [{
     supportedMethods: ['basic-card'],
     data: {
-      supportedNetworks: ['visa', 'mastercard', 'amex', 'discover', 'diners', 'jcb', 'unionpay']
+      supportedNetworks: ['visa', 'mastercard', 'amex', 'discover','diners', 'jcb', 'unionpay']
     }
-  }];
+  }]
 
   /* 支払い詳細 */
-  var detailParams = {
+  const detailParams = {
     displayItems: [{
       label: '本体価格',
       amount: { currency: 'JPY', value: '12800' }
@@ -29,54 +29,56 @@ function clickPayment() {
       label: '通常配送',
       amount: { currency: 'JPY', value: '0' },
       selected: true
-    }, {
+    },{
       id: 'express',
       label: '特急配送',
       amount: { currency: 'JPY', value: '500' }
     }],
     total: {
       label: '合計価格',
-      amount: { currency: 'JPY', value: '12800' }
+      amount: { currency: 'JPY', value : '12800' }
     }
+  }
 
-    /* 支払いオプション */
-  };var options = {
+  /* 支払いオプション */
+  const options = {
     requestShipping: true,
     shippingType: 'delivery'
-  };
+  }
 
-  var request = new PaymentRequest(methodData, detailParams, options);
+  const request = new PaymentRequest(methodData, detailParams, options)
 
   /* 住所変更の地道な処理 */
-  request.addEventListener('shippingaddresschange', function (e) {
-    e.updateWith(function (detailParams) {
+  request.addEventListener('shippingaddresschange', e => {
+    e.updateWith(((detailParams) => {
       /* 自力でPromiseを解決しておかないとUIが死ぬ */
-      return Promise.resolve(detailParams);
-    }(detailParams));
-  });
+      return Promise.resolve(detailParams)
+    })(detailParams))
+  })
 
   /* 配送料変更の地道な処理 */
-  request.addEventListener('shippingoptionchange', function (e) {
-    e.updateWith(function (detailParams, shippingOption) {
-      if (shippingOption === 'standard') {
+  request.addEventListener('shippingoptionchange', e => {
+    e.updateWith(((detailParams, shippingOption) => {
+      if (shippingOption==='standard') {
         /* なんでここの処理が人力なのか謎 */
-        detailParams.shippingOptions[0].selected = true;
-        detailParams.shippingOptions[1].selected = false;
-        detailParams.displayItems[1].amount.value = '0';
+        detailParams.shippingOptions[0].selected = true
+        detailParams.shippingOptions[1].selected = false
+        detailParams.displayItems[1].amount.value = '0'
 
-        detailParams.total.amount.value = '12800';
+        detailParams.total.amount.value = '12800'
       } else {
-        detailParams.shippingOptions[0].selected = false;
-        detailParams.shippingOptions[1].selected = true;
-        detailParams.displayItems[1].amount.value = '500';
+        detailParams.shippingOptions[0].selected = false
+        detailParams.shippingOptions[1].selected = true
+        detailParams.displayItems[1].amount.value = '500'
 
-        detailParams.total.amount.value = '13200';
+        detailParams.total.amount.value = '13200'
       }
-      return Promise.resolve(detailParams);
-    }(detailParams, request.shippingOption));
-  });
+      return Promise.resolve(detailParams)
+    })(detailParams, request.shippingOption))
+  })
 
-  request.show().then(function (result) {
+  request.show()
+  .then(result => {
     /* どこか支払い処理してくれるAPIにPOSTする */
     return fetch('/', {
       method: 'POST',
@@ -85,18 +87,18 @@ function clickPayment() {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(result.toJSON())
-    }).then(function (response) {
+    }).then(response => {
       if (response.status === 200) {
         /* 支払い成功 */
-        return result.complete('success');
+        return result.complete('success')
       } else {
         /* 支払い失敗 */
-        return result.complete('fail');
+        return result.complete('fail')
       }
-    }).catch(function () {
+    }).catch(() => {
       /* 支払い失敗 */
-      return result.complete('fail');
-    });
-  });
+      return result.complete('fail')
+    })
+  })
 }
-document.querySelector('.paymentBtn').addEventListener('click', clickPayment);
+document.querySelector('.paymentBtn').addEventListener('click', clickPayment)
